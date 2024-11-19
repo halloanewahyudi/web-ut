ac2a7b975ed4771edf4687d1cec4a2acd650b159
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-11-19 20:27:29 +0700

    section

--- src/components/sectionFive.vue
+++ src/components/sectionFive.vue
@@ -2,8 +2,9 @@
     <section class="section min-h-screen flex flex-col justify-center items-center py-16">
          <div class="container flex flex-col gap-5">
             <div class="max-w-xl mx-auto text-center group">
-                <h2 class="title text-5xl lg:text-6xl xl:text-8xl mb-5 hover:text-brand-blue"> <a href="#">One Commitment </a> </h2>
-                <p>United Tractors berkomitmen menjadi mitra terpercaya bagi pelanggan dengan memberikan solusi dan nilai tambah. United Tractors selalu berfokus pada keberlanjutan jangka panjang dalam implementasi ESG untuk membangun masa depan yang lebih baik.
+                <h2 class="title text-5xl lg:text-6xl xl:text-8xl mb-5 hover:text-brand-blue"> <a href="#">One Spirit </a> </h2>
+                <p>
+                    United Tractors berdedikasi melayani pelanggan dengan sepenuh hati dan mengutamakan kerja sama. United Tractors mengedepankan kolaborasi dengan semua pemangku kepentingan dan menciptakan praktik berkelanjutan jangka panjang melalui produk, layanan, dan semua lini bisnis kami.
                 </p>
             </div>
             <div class="max-w-screen-xl mx-auto">
