49cae18e9951ab84b1a47137e338e5c7d559ab69
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-11-20 07:59:31 +0700

    lagi

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  },
   build: {
     sourcemap: true, // Tambahkan sourcemap untuk debugging
     target: 'esnext', // Pastikan menggunakan modern ES
